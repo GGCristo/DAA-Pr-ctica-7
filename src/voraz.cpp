af3b27e800fbd7cc344b22ba22fb4ae07f3d302c
Cristo <alu0101204512@ull.edu.es>
2021-04-11 15:19:28 +0100

    Fix posiciones iniciales

--- src/voraz.cpp
+++ src/voraz.cpp
@@ -7,26 +7,28 @@ void voraz(std::vector<Maquina>& maquinas) {
 }
 
 const Tarea getTask(int previousTask) {
+  int auxMinSum;
   int minPosition = -1;
   Tarea tarea;
   Datos* datos = &Datos::getInstance();
   size_t i = 1;
   for (; i < datos->getSetups()[previousTask].size(); ++i) {
-    if (!datos->getTimes()[i].second) {
-      minPosition = i;
+    if (!datos->getTimes()[i - 1].second) {
+      auxMinSum = datos->getTimes()[i - 1].first +
+        datos->getSetups()[previousTask][i];
+      minPosition = i++;
       break;
     }
   }
   for(; i < datos->getSetups()[previousTask].size(); ++i) {
-    if (!datos->getTimes()[i].second &&
-        datos->getSetups()[previousTask][i] <
-        datos->getSetups()[previousTask][minPosition]) {
+    if (!datos->getTimes()[i - 1].second &&
+        datos->getTimes()[i -1].first + datos->getSetups()[previousTask][i] <
+        auxMinSum) {
+      auxMinSum = datos->getTimes()[i - 1].first +
+        datos->getSetups()[previousTask][i];
       minPosition = i;
     }
   }
-  // std::cout << "Meto: MinPosition; " << minPosition << " Times; " << datos->getSetups()[previousTask][minPosition] << '\n';
-  datos->getTimes()[minPosition].second = true;
-  return Tarea(minPosition,
-      datos->getSetups()[previousTask][minPosition] +
-      datos->getTimes()[minPosition].first);
+  datos->getTimes()[minPosition - 1].second = true;
+  return Tarea(minPosition, auxMinSum);
 }
